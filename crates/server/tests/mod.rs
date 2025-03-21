e190f3cb66aaaa4ac059e5733faeede5eec37158
shunm-999 <shunmiyazaki7812@gmail.com>
2025-03-21 15:26:09 +0900

    add useless code

--- crates/server/tests/mod.rs
+++ crates/server/tests/mod.rs
@@ -1,32 +1,9 @@
 pub(crate) mod client;
 pub(crate) mod endpoint;
 
-use actix_http::body::BoxBody;
-use actix_http::Request;
 use actix_web::dev::{Service, ServiceResponse};
-use actix_web::web::Data;
-use actix_web::{test, App};
-use infra::SmartTaskRepositoryImpl;
+use actix_web::test;
 use serde::de::DeserializeOwned;
-use server::AppData;
-use std::sync::Arc;
-
-// pub(crate) async fn config<S, B>() -> S
-// where
-//     S: Service<Request, Response = ServiceResponse<BoxBody>, Error = B>,
-//     B: std::fmt::Debug,
-// {
-//     let repository = SmartTaskRepositoryImpl::new("sqlite::memory:".to_string()).await;
-//     let app_data = Data::new(AppData {
-//         repository: Arc::new(repository),
-//     });
-//     let app = App::new().app_data(app_data.clone());
-//
-//     test::init_service(app.configure(|cfg| {
-//         server::endpoint::config(cfg);
-//     }))
-//     .await
-// }
 
 pub struct TestResponse {
     pub response: ServiceResponse,
